5308ea8e047d63fcab616eecb2943a9059dcb5a9
jorjeGs <jgs_23072000@outlook.com>
2024-04-22 21:59:24 -0700

    styling item i guess

--- app/(tabs)/items/[id].jsx
+++ app/(tabs)/items/[id].jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, Image, ScrollView, SafeAreaView, StyleSheet, ActivityIndicator } from 'react-native'
-import { useLocalSearchParams } from 'expo-router'
-import { getDatabase, ref, onValue } from 'firebase/database'
+import { Text, View, Image, ScrollView, SafeAreaView, StyleSheet, ActivityIndicator, TextInput } from 'react-native'
+import { router, useLocalSearchParams } from 'expo-router'
+import { getDatabase, ref, onValue, update, set } from 'firebase/database'
+import Toast from 'react-native-toast-message'
 
 const Item = () => {
   const { id } = useLocalSearchParams()
+  const [originalItem, setOriginalItem] = useState({})
   const [item, setItem] = useState({})
   const [isLoading, setIsLoading] = useState(true)
 
@@ -12,52 +14,122 @@ const Item = () => {
     const db = getDatabase()
     onValue(ref(db, `items/${id}`), (snapshot) => {
       const data = snapshot.val()
-      console.log(data)
       setItem(data)
+      setOriginalItem(data)
       setIsLoading(false)
-    },
-    {
-      onlyOnce: true
+    }, (error) => {
+      Toast.show({
+        type: 'error',
+        text1: 'Error al obtener el item',
+        text2: error.message
+      })
+      router.back()
+      console.log('Error: ' + error)
+      setIsLoading(false)
     })
+  }
+
+  const handleItemInfo = (value, name) => {
+    setItem({ ...item, [name]: value })
+  }
+
+  const updateItem = () => {
+    const db = getDatabase()
+    setIsLoading(true)
+    update(ref(db, `items/${id}`), item)
+      .then(() => {
+        setIsLoading(false)
+        setOriginalItem(item)
+        Toast.show({
+          type: 'success',
+          text1: 'Item actualizado!',
+          onPress: () => Toast.hide()
+        })
+        router.back()
+      })
+      .catch((error) => {
+        Toast.show({
+          type: 'error',
+          text1: 'Error al actualizar el item',
+          text2: error.message
+        })
+        console.log('Data could not be updated.', error)
+        setIsLoading(false)
+      })
   }
 
   useEffect(() => {
     getItem()
   }, [id])
 
+  const onFocus = (styleName) => {
+    console.log(styleName)
+    if (styles[styleName].borderColor === 'transparent') {
+      console.log('setting border color')
+      styles[styleName].borderColor = 'gray'
+    } else {
+      styles[styleName].borderColor = 'transparent'
+    }
+    console.log(styles[styleName].borderColor)
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView style={{ flex: 1, flexDirection: 'column', gap: 20, padding: 20 }}>
         {isLoading && <ActivityIndicator style={{ left: 0, right: 0, bottom: 0, top: 0, position: 'absolute', zIndex: 9999 }} size='large' color='black' />}
-        <Text style={styles.textName}>{item.name}</Text>
+        <TextInput
+          style={[styles.textName]}
+          onChangeText={(value) => handleItemInfo(value, 'name')}
+          value={item.name}
+          onFocus={() => onFocus('textName')}
+        />
         <View style={{ width: '100%', height: 300 }}>
-          <Image style={{ height: '100%', width: '100%', marginRight: 'auto', marginLeft: 'auto', objectFit: 'contain' }} source={{ uri: item.image }} />
+          <Image style={{ height: '80%', width: '80%', marginRight: 'auto', marginLeft: 'auto', marginTop: 'auto', marginBottom: 'auto', objectFit: 'contain' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/3134/3134241.png' }} />
         </View>
-        <Text style={styles.textDescription}>{item.description}</Text>
-        <View style={{ flex: 1, flexDirection: 'row', marginTop: 10 }}>
-          <Text>Quantity:</Text>
-          <Text>{1}</Text>
+        <View style={{ flex: 1, flexDirection: 'column', marginTop: 10, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 16 }}>Descripción</Text>
+          <TextInput
+            style={[styles.textDescription]}
+            onChangeText={(value) => handleItemInfo(value, 'description')}
+            value={item.description}
+            onFocus={() => onFocus('textDescription')}
+          />
+        </View>
+        <View style={{ flex: 1, flexDirection: 'column', marginTop: 30, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={[styles.textQty, { }]}>Cantidad</Text>
+          <Text style={styles.textQtyValue}>{item.qty}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
+let styles = StyleSheet.create({
   textName: {
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 20
+    marginTop: 20,
+    borderBottomWidth: 2,
+    padding: 2,
+    borderColor: 'transparent'
   },
   textQtyValue: {
-    fontSize: 20
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontSize: 40
+  },
+  textQty: {
+    fontSize: 16
   },
   textDescription: {
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 20
+    marginTop: 10,
+    borderBottomWidth: 2,
+    padding: 2,
+    borderColor: 'transparent'
   }
 })
 
