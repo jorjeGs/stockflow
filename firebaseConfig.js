d8fcfe4dc26839f0961882ce610addd572f7e6b0
jorjeGs <jgs_23072000@outlook.com>
2024-04-30 21:37:12 -0700

    Export

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -19,6 +19,6 @@ const firebaseConfig = {
 }
 
 export const app = initializeApp(firebaseConfig)
-const db = getDatabase(app)
+export const db = getDatabase(app)
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
